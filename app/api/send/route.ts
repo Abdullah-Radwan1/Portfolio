729808222e71d4ab52b7ebbdb691f19e798adf0a
Abdallah <abdallahbeedo855@gmail.com>
2024-10-11 21:39:58 +0300

    first commit

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,24 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { EmailTemplate } from "@/components/email-template";
 import { Resend } from "resend";
-import * as React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
- try {
-  const { data, error } = await resend.emails.send({
-   from: "Acme <onboarding@resend.dev>",
-   to: ["delivered@resend.dev"],
-   subject: "Hello world",
-   react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
-  });
+// Replace default export with a named POST handler
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+ const { data, error } = await resend.emails.send({
+  from: "Acme <onboarding@resend.dev>",
+  to: ["delivered@resend.dev"],
+  subject: "Hello world",
+  react: EmailTemplate({ firstName: "John" }),
+ });
 
-  if (error) {
-   return Response.json({ error }, { status: 500 });
-  }
-
-  return Response.json({ data });
- } catch (error) {
-  return Response.json({ error }, { status: 500 });
+ if (error) {
+  return res.status(400).json(error);
  }
+ console.log(data);
+ res.status(200).json(data);
 }
