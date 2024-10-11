cd2bd08b32fe138d88ea37489ba8f0912a7eaf58
Abdallah <abdallahbeedo855@gmail.com>
2024-10-11 22:27:44 +0300

    first commit

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { EmailTemplate } from "@/components/email-template";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.API_KEY);
-console.log(process.env.API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
+console.log("dsds", process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
  try {
@@ -12,14 +12,14 @@ export async function POST(req: NextRequest) {
   console.log("Request Body:", requestBody); // Log the request body
 
   // Extract necessary data from the request body
-  const { subject, to } = requestBody; // Assuming your body contains `firstName` and `to`
+  const { subject, message } = requestBody; // Assuming your body contains `firstName` and `to`
 
   // Send email using the extracted data
   const { data, error } = await resend.emails.send({
    from: "Acme <onboarding@resend.dev>",
-   to: to || ["abdallahbeedo855@gmail.com"], // Use provided recipient or fallback
+   to: ["abdallahbeedo855@gmail.com"], // Use provided recipient or fallback
    subject: requestBody.subject,
-   react: EmailTemplate({ subject }), // Use firstName from the request body
+   react: EmailTemplate({ subject, message }), // Use firstName from the request body
   });
 
   if (error) {
--- components/email-template.tsx
+++ components/email-template.tsx
@@ -2,10 +2,12 @@ import * as React from "react";
 
 interface EmailTemplateProps {
  subject: string;
+ message: string;
 }
 
-export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ subject }) => (
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ subject, message }) => (
  <div>
-  <h1> {subject}!</h1>
+  <h1> {subject}</h1>
+  <p> {message}</p>
  </div>
 );
