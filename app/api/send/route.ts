4737f94f6534db766f71b722cf6a609479b076ba
abdullah <abdallahbeedo855@gmail.com>
2025-03-21 08:53:40 +0200

    fixing email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -6,30 +6,33 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 console.log("dsds", process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
- try {
-  // Parse the JSON body of the request
-  const requestBody = await req.json();
-  console.log("Request Body:", requestBody); // Log the request body
+  try {
+    // Parse the JSON body of the request
+    const requestBody = await req.json();
+    console.log("Request Body:", requestBody); // Log the request body
 
-  // Extract necessary data from the request body
-  const { subject, message } = requestBody; // Assuming your body contains `firstName` and `to`
+    // Extract necessary data from the request body
+    const { subject, message } = requestBody; // Assuming your body contains `firstName` and `to`
 
-  // Send email using the extracted data
-  const { data, error } = await resend.emails.send({
-   from: "Acme <onboarding@resend.dev>",
-   to: ["abdallahbeedo855@gmail.com"], // Use provided recipient or fallback
-   subject: requestBody.subject,
-   react: EmailTemplate({ subject, message }), // Use firstName from the request body
-  });
+    // Send email using the extracted data
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["abdallahbeedo855@gmail.com"], // Use provided recipient or fallback
+      subject: requestBody.subject,
+      react: EmailTemplate({ subject, message }),
+    });
 
-  if (error) {
-   return NextResponse.json(error, { status: 400 });
+    if (error) {
+      return NextResponse.json(error, { status: 400 });
+    }
+
+    console.log("Email Sent Data:", data); // Log the response data
+    return NextResponse.json(data, { status: 200 });
+  } catch (err) {
+    console.error("Error:", err); // Log the error for debugging
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
   }
-
-  console.log("Email Sent Data:", data); // Log the response data
-  return NextResponse.json(data, { status: 200 });
- } catch (err) {
-  console.error("Error:", err); // Log the error for debugging
-  return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
- }
 }
--- components/email-template.tsx
+++ components/email-template.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 
 interface EmailTemplateProps {
- subject: string;
- message: string;
+  subject: string;
+  message: string;
 }
 
-export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ subject, message }) => (
- <div>
-  <h1> {subject}</h1>
-  <p> {message}</p>
- </div>
+export const EmailTemplate = ({ subject, message }: EmailTemplateProps) => (
+  <div>
+    <h1> {subject}</h1>
+    <p> {message}</p>
+  </div>
 );
